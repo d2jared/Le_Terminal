dd9ef0d9260408b711810524ded33d350d8f9af3
d2jared <75532854+d2jared@users.noreply.github.com>
2023-04-12 11:38:49 +0200

    système d'inscriptions v2
    le dernière version du système d'inscriptions (la plus stable)

---
+++ Le terminal/Le terminal/signup.php
@@ -0,0 +1,33 @@
+<?php require('actions/signupAction.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include "includes/head.php"; ?>
+<body>
+
+<?php  if(isset($errorMsg)){ echo '<p>' .$errorMsg. '<p>'; } ?>
+
+<!--formulaire-->
+<br />
+<br />
+<form class="container" method="POST">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Pseudo</label>
+    <input type="text" class="form-control" name="pseudo">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Nom</label>
+    <input type="text" class="form-control" name="lastname">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Prénom</label>
+    <input type="text" class="form-control" name="firstname">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Mot De Passe</label>
+    <input type="password" class="form-control" name="password">
+  </div>
+  <button type="submit" class="btn btn-primary" name="validate">S'inscrire</button>
+</form>
+    
+</body>
+</html>
